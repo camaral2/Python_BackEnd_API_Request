437a314ce113aba77cb46f2f0583774b808813c0
camaral2 <cristian_amaral@hotmail.com>
2025-05-05 12:07:23 -0300

    test: Testing mock http request

--- src/util/get_api_consumer.py
+++ src/util/get_api_consumer.py
@@ -1,10 +1,14 @@
 import requests
 
 class GetApiConsumer:
-    
+    '''Get info API'''
     @classmethod
     def get_starships(self, page: int) -> any:
+        '''Get starships from API'''
         params = { 'page':page }
-        resp = requests.get('https://swapi.dev/api/starships/', params=params, verify=False)
+        resp = requests.get('https://swapi.dev/api/starships/', 
+                            params=params, 
+                            verify=False,
+                            timeout=10)
 
         return resp.json()
--- src/util/get_api_consumer_test.py
+++ src/util/get_api_consumer_test.py
@@ -1,7 +1,8 @@
 from .get_api_consumer import GetApiConsumer
 
-def test_get_starships():
+def test_get_starships(requests_mock):
     ''' Testing get_starships method'''
+    requests_mock.get('https://swapi.dev/api/starships/', status_code = 200, json={ 'some': 'thing' })
     
     api_consumer =  GetApiConsumer()
     resp = api_consumer.get_starships(page=1)
